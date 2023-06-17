fb4a6b41c4f01dfd1150352f7a95bd1fe55a0c2b
Nachuan Wang <1187014311@qq.com>
2023-06-16 19:36:51 -0700

    Update design note

--- doc/design.md
+++ doc/design.md
@@ -9,31 +9,59 @@ npm run local     // commit to local folder
 npm run build     // compile code
 ```
 
-## **Thread**
+## **General Idea**
 ---
-1. 所有对游戏对象的操作都应该通过thread完成
-2. 每个RoomManagerProcess都下辖以下功能的thread:
-    - 标定ConstructionSite
-    - 为ConstructionSite发布建造任务
-    - 遍历所有Structure对象，进行必要操作或发布任务
-        - Task log应该挂在Structure的内存里
-    - 遍历所有Worker Creep对象，获取并执行任务
-3. Thread有0-2的priority，在操作对像前先把对象的priority field设为自己的priority，操作完成后再恢复原priority，通过这种机制来抢占资源
-    - 抢占是否会中断当前任务还有待商榷
+1. 观摩Tigga运营得出的经验：harvester/updator站桩工作，transporter在中间搬运资源
+    - 理由：为work部件添加move部件是不值得的
+2. 所以任务调度系统过于general了，应该用一个结构更简单的物流系统代替
+3. 建造和修理任务可以独立于物流系统
 
-## **Task**
+## **Process & Thread**
 ---
-1. task不用放回task queue中
-2. 每个task的发布者（thread/target）都应保存一个task log，task结束时无论task完成与否，log上对应的记录都要被删除
-    - 派生的task没有id，也不需要在结束时更新log
-    - 发布者应该自己确认task是否完成，如果task结束但未完成则需要重新发布task
-3. 每次harvest都采满再走，暂时不处理采满但存完还剩一点的情况，之后统一用一个dump任务处理
+1. 所有需要状态机且不直接操作游戏对象的逻辑应该用process & thread完成
+2. Thread有0-2的priority，在操作对像前先把对象的priority field设为自己的priority，操作完成后再恢复原priority，通过这种机制来抢占资源
+    - 抢占是否会中断当前任务还有待商榷
+3. 部分process/thread会直接控制creep（任务系统无法满足战斗的需求）
+4. RoomManagerProcess包含以下功能的thread
+    - 标定ConstructionSite
+    - 房间运营，发布非军事creep的spawn request
+    - 保护房间安全，发布军事creep的spawn request并控制它们战斗
+
+## **Transportation System**
+---
+1. 物流系统由node和transporter组成：node可以是任何需要搬入/搬出物资的对象；transporter是专门运输资源的creep
+2. Node可以发布请求资源的task
+    - 每个task包含如下信息
+        - owner：信息发布者的id
+        - srcType：请求资源的类型
+        - tarAmount：请求资源的数量（如果是0则代表不设上限）
+        - resAmount：已经预定的额度（如果tarAmount==0则一直为0）
+        - comAmount：已经完成的额度（如果tarAmount==0则一直为0）
+    - 每个transporter不会认领整个task，而是根据自己的运载能力预定任务额度，完成后再去更新已完成的额度
+        - 每次预定都会生成一个package分配给transporter，之后transporter会专注于这个package直到完成
+    - 在没有task时，transporter会主动寻找掉落在地上的资源并带在身上，这会生成一个特殊类型的transaction
+        -  之所以要和普通transaction作区分是因为这只涉及捡起不涉及运输
+3. 每个房间都有一个task.trans内存对象: 包含房间内所有的运输任务，有0-2三个优先级（数字越小优先级越高）
+4. 每个房间都有一个srcNode: 建筑类型（包括掉落的资源）分类的可以取用资源的node集合
+    - srcNode集合和Construction System共享
+    - 需要有专门的thread更新掉落资源的状态
+5. 目标node并不需要一个数据结构来表达，因为task已经足够了
+
+## **Construction System**
+---
+1. 建造系统由node和worker组成：node可以是任何construction site或许需要修理的建筑，worker是专门负责建造/维修的creep
+    - 和物流系统几乎相同，区别是给出能量的方法不同
+2. 每个房间都有一个task.build内存对象: 包含房间内所有的运输任务，有0-2三个优先级（数字越小优先级越高）
+3. 和物流系统共用srcNode，transaction数据结构和调度函数
+
+## **Execution Order**
+---
+1. Process/Thread发布任务，thread直接控制的creep执行操作
+2. Scheduler分配任务
+3. 剩余所有creep执行操作
 
 ## **TODO List**
 ---
-1. 改进spawn索取能量的逻辑
-2. 分配能量的事情由structure自行发布任务完成，agent不参与
-3. 用Addr生成对象时应检查cache中是否已存在此对象，同样的每次生成后都应把对象挂到cache上（用一个cache函数封装这个功能）
-4. 用一个cache函数封装build功能，输入addr它就能根据type生成对应的object
+
 
 
--- src/type/memory.d.ts
+++ src/type/memory.d.ts
@@ -14,58 +14,64 @@ declare global {
 }
 
 interface GlobalMemory {
-    struct: RoomStructureMemory;
-    process: {[name: string]: ProcessMemory};
+    creep: CreepsMemory;
+    struct: StructuresMemory;
+    proc: {[name: string]: ProcessMemory};
 }
 
 interface RoomMemory {
-    struct: RoomStructureMemory;
-    source: {[id: string]: {attach: number;}};
-    taskQueue: any;
-    spawnQueue: any;
-    process: {[name: string]: ProcessMemory};
+    creep: CreepsMemory;
+    struct: StructuresMemory;
+    src: {id: string; attach: number;}[];
+    srcDrop: {};        // TODO
+    task: {
+        trans: any;
+        build: any;
+        spawn: any;
+    };
+    proc: {[name: string]: ProcessMemory};
 }
 
+interface CreepsMemory {
+    harvester: CreepMemory[];
+    worker: CreepMemory[];
+    transporter: CreepMemory[];
+    upgrader: CreepMemory[];
+}
+
+// Should have different interface for different creeps
 interface CreepMemory {
     id: string;
-    role: string;
-    owned: boolean;             // True if the creep is owned by a process
-    prio: number;               // Priority of the process that owns the creep
-    task: TaskMemory | null;    // Task that the creep is executing
+    owner: number;      // 0-2 (same as priority) if owned by a process/thread, -1 if not
+    state: number;      // Working state
+    pkg?: any;          // TODO
 }
 
-interface RoomCreepMemory {
-    harvester: {[id: string]: CreepMemory};
-    worker: {[id: string]: CreepMemory};
-    transporter: {[id: string]: CreepMemory};
-    attacker: {[id: string]: CreepMemory};
-    healer: {[id: string]: CreepMemory};
+interface StructuresMemory {
+    spawn: {id: string}[];
+    extension: {id: string}[];
+    road: {id: string}[];
+    constructedWall: {id: string}[];
+    rampart: {id: string}[];
+    keeperLair: {id: string}[];
+    portal: {id: string}[];
+    controller: {id: string}[];
+    link: {id: string}[];
+    storage: {id: string}[];
+    tower: {id: string}[];
+    observer: {id: string}[];
+    powerBank: {id: string}[];
+    powerSpawn: {id: string}[];
+    extractor: {id: string}[];
+    lab: {id: string}[];
+    terminal: {id: string}[];
+    container: {id: string}[];
+    nuker: {id: string}[];
+    factory: {id: string}[];
+    invaderCore: {id: string}[];
 }
 
 interface StructureMemory {
-    taskLog: TaskLog;
-}
+    id: string;
 
-interface RoomStructureMemory {
-    spawn: {[id: string]: StructureMemory};
-    extension: {[id: string]: StructureMemory};
-    road: {[id: string]: StructureMemory};
-    constructedWall: {[id: string]: StructureMemory};
-    rampart: {[id: string]: StructureMemory};
-    keeperLair: {[id: string]: StructureMemory};
-    portal: {[id: string]: StructureMemory};
-    controller: {[id: string]: StructureMemory};
-    link: {[id: string]: StructureMemory};
-    storage: {[id: string]: StructureMemory};
-    tower: {[id: string]: StructureMemory};
-    observer: {[id: string]: StructureMemory};
-    powerBank: {[id: string]: StructureMemory};
-    powerSpawn: {[id: string]: StructureMemory};
-    extractor: {[id: string]: StructureMemory};
-    lab: {[id: string]: StructureMemory};
-    terminal: {[id: string]: StructureMemory};
-    container: {[id: string]: StructureMemory};
-    nuker: {[id: string]: StructureMemory};
-    factory: {[id: string]: StructureMemory};
-    invaderCore: {[id: string]: StructureMemory};
 }
